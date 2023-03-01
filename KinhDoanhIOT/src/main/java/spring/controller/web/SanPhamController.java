4cfbab31267e755a87b237876cec43e8fb9b291b
lekhoi0702 <lekhoi0702@gmail.com>
2023-03-01 13:44:23 +0700

    update contact

--- KinhDoanhIOT/src/main/java/spring/controller/web/SanPhamController.java
+++ KinhDoanhIOT/src/main/java/spring/controller/web/SanPhamController.java
@@ -17,23 +17,34 @@ import spring.dto.SanPhamDTO;
 
 
 @Controller
+@RequestMapping(value = "/nhanvien-home/")
 public class SanPhamController {
 
+	//CONTROLLER
+	@RequestMapping(value = "nhanvien-qlsanpham", method = RequestMethod.GET)
+	public <E> String showSanPham(HttpServletRequest request, ModelMap model) {
 
-	@RequestMapping(value = "sanpham", method = RequestMethod.GET)
-	public String showMenu(ModelMap model, HttpServletRequest request) throws IOException {
-		
-		List<SanPhamDTO> list = Collector.getListAll("/sanpham",SanPhamDTO.class);
+		List<SanPhamDTO> list = null;
+		try {
+			list = Collector.getListAll("/sanpham", SanPhamDTO.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		model.addAttribute("list", list);
-		return "web/sanpham";
+		return "nhanvien/qlsanpham";
+	}
+	
+	/* hiển thị form */
+	@RequestMapping(value = "formSanPham", method = RequestMethod.GET)
+	public String index_formSanPham(ModelMap model) {
+		model.addAttribute("td", new SanPhamDTO());
+
+		model.addAttribute("loaisps", getLoaiSPs());
+
+		return "admin/form/inputSanPham";
 	}
 
-	@RequestMapping(value = "sanpham", params="btnsearch")
-	public <E> String showMenu2(ModelMap model, HttpServletRequest request) {
-		
-	
-		return "web/sanpham";
-	}
 	
 
 	
