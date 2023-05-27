ddcf2c2c36b7a7b258f3495d099a3ecb26e34276
thuanntd2001 <thiensudanduong12@gmail.com>
2023-05-27 15:23:48 +0700

    fix tim kiem

--- KinhDoanhIOT/src/main/java/spring/controller/khachhang/TimKiemController.java
+++ KinhDoanhIOT/src/main/java/spring/controller/khachhang/TimKiemController.java
@@ -3,6 +3,7 @@ package spring.controller.khachhang;
 import java.io.IOException;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -36,27 +37,33 @@ public class TimKiemController {
 
 		return "khachhang/home";
 	}
-	
+
 	@RequestMapping(value = "khachhangtimkiemtukhoa", method = RequestMethod.GET)
 	public String sanPham_TuKhoa(HttpServletRequest request, ModelMap model) {
 		List<SanPhamDTO> sps2 = new ArrayList<SanPhamDTO>();
 		List<SanPhamDTO> sps1 = this.getSanPhams();
 		String tenTuKhoa = this.removeAccent(request.getParameter("searchtukhoa").toLowerCase());
-		List<Long> masps= this.getMaSPTheoTuKhoa(tenTuKhoa);
+		List<Long> masps = this.getMaSPTheoTuKhoa(tenTuKhoa);
 
-		//List<TuKhoa_NhanDTO> tuKhoa_Nhans=this.getNhanTuKhoa(tenTuKhoa);
-		//sps2 = this.locTheoNhan(tenNhan);
-		String tenTuKhoas[] = tenTuKhoa.split(" ",0);
-		for (SanPhamDTO sp :sps1) {
-			System.out.println("test: "+tenTuKhoas[0].toString()+" "+sp.getTen());
+		// List<TuKhoa_NhanDTO> tuKhoa_Nhans=this.getNhanTuKhoa(tenTuKhoa);
+		// sps2 = this.locTheoNhan(tenNhan);
+		String tenTuKhoas[] = tenTuKhoa.split(" ", 0);
+		for (SanPhamDTO sp : sps1) {
 
-			if (this.locTheoTenSP(tenTuKhoas, sp.getTen()) == 1 /* || masps.contains(sp.getID()) */ ) {
+			if (this.locTheoTenSP(tenTuKhoas, sp.getTen()) == true  ) {
 				sps2.add(sp);
 			}
 		}
+		for (SanPhamDTO sp :sps1) {
+			System.out.println("test: "+tenTuKhoas[0].toString()+" "+sp.getTen());
 
-		
-		model.addAttribute("SanPhams", sps2);
+			if (masps.contains(sp.getID()) ) {
+				sps2.add(sp);
+			}
+			
+		}
+		List<SanPhamDTO> sps3=this.removeDuplicates(sps2);
+		model.addAttribute("SanPhams", sps3);
 		model.addAttribute("LoaiSPs", getLoaiSPs());
 
 		return "khachhang/home";
@@ -72,7 +79,7 @@ public class TimKiemController {
 		}
 		return list;
 	}
-	
+
 	public List<TuKhoa_NhanDTO> getNhanTuKhoa(String tenTuKhoa) {
 		List<TuKhoa_NhanDTO> list = null;
 		try {
@@ -139,22 +146,31 @@ public class TimKiemController {
 
 		return sps2;
 	}
-	public int locTheoTenSP(String tuKhoa[], String tenSP) {
-		String tenSPs[] = this.removeAccent(tenSP.toLowerCase()).split(" ", 0);
-		
-		for (int i=0; i<tuKhoa.length;i++) 
-			for (int j=0; j<tenSPs.length;j++) 
-		{
-			System.out.println(tuKhoa[i]+":"+tenSPs[j]+" KQ:"+tuKhoa[i].equals(tenSPs[j]));
-			if (tuKhoa[i].equals(tenSPs[j])){
-				return 1;
+
+	public boolean locTheoTenSP(String tuKhoa[], String tenSP) {
+		String tenSPs = this.removeAccent(tenSP.toLowerCase());
+
+		for (int i = 0; i < tuKhoa.length; i++) {
+			if (tenSPs.contains(tuKhoa[i])) {
+				return true;
 			}
 		}
 
-		return 0;
+		return false;
 	}
-	
-	
+
+	public ArrayList<SanPhamDTO> removeDuplicates(List<SanPhamDTO> sps2) {
+		HashSet<Long> set = new HashSet<>();
+		ArrayList<SanPhamDTO> newList = new ArrayList<>();
+		for (SanPhamDTO sp : sps2) {
+			if (!set.contains(sp.getID())) { // giả sử getMaSP() là phương thức để lấy mã sản phẩm
+				set.add(sp.getID());
+				newList.add(sp);
+			}
+		}
+		return newList;
+	}
+
 	public List<LoaiSPDTO> getLoaiSPs() {
 		List<LoaiSPDTO> list = null;
 		try {
@@ -164,32 +180,32 @@ public class TimKiemController {
 			e.printStackTrace();
 		}
 		return list;
-	} 
-	
+	}
+
 	public List<Long> getMaSPTheoTuKhoa(String tuKhoa) {
 		List<TuKhoa_NhanDTO> listTuKhoaNhan = new ArrayList<TuKhoa_NhanDTO>();
 		List<Nhan_SanPhamDTO> listNhanSP = new ArrayList<Nhan_SanPhamDTO>();
-		List<Long> listMaSP=new ArrayList<Long>();
-		if (tuKhoa==null || tuKhoa=="") {
+		List<Long> listMaSP = new ArrayList<Long>();
+		if (tuKhoa == null || tuKhoa == "") {
 			return listMaSP;
 		}
 		try {
 			listTuKhoaNhan = Collector.getListAll("/tukhoanhan", TuKhoa_NhanDTO.class);
-			//listNhanSP = Collector.getListAll("/loaisp", Nhan_SanPhamDTO.class);
+			// listNhanSP = Collector.getListAll("/loaisp", Nhan_SanPhamDTO.class);
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		String temp;
-		tuKhoa=this.removeAccent(tuKhoa.toLowerCase());
+		tuKhoa = this.removeAccent(tuKhoa.toLowerCase());
 		for (TuKhoa_NhanDTO tkn : listTuKhoaNhan) {
-			temp=this.removeAccent(tkn.getTuKhoa().toLowerCase());
-			
-			if (tuKhoa.indexOf(temp)!=-1 || temp.indexOf(tuKhoa)!= -1) {
+			temp = this.removeAccent(tkn.getTuKhoa().toLowerCase());
+
+			if (tuKhoa.indexOf(temp) != -1 || temp.indexOf(tuKhoa) != -1) {
 				try {
-					
-					listNhanSP = Collector.getListAll("/nhansanpham?tennhan="+tkn.getNhan(), Nhan_SanPhamDTO.class);
+
+					listNhanSP = Collector.getListAll("/nhansanpham?tennhan=" + tkn.getNhan(), Nhan_SanPhamDTO.class);
 					for (Nhan_SanPhamDTO nsp : listNhanSP) {
 						listMaSP.add(nsp.getSanPham());
 					}
@@ -198,13 +214,17 @@ public class TimKiemController {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
 		}
 		return listMaSP;
-	} 
-	
-	public String removeAccent(String s) { String temp = Normalizer.normalize(s, Normalizer.Form.NFD); Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+"); temp = pattern.matcher(temp).replaceAll(""); 
-    return temp.replaceAll("đ", "d"); }
+	}
+
+	public String removeAccent(String s) {
+		String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
+		Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+		temp = pattern.matcher(temp).replaceAll("");
+		return temp.replaceAll("đ", "d");
+	}
 
 }
